d83df46dc3c5d6d1e0fbe9048803cc83a615f0d1
Ali-Akber Saifee <ali@indydevs.org>
2013-12-03 10:52:16 +0800

    tweaks to pyenv segment

--- _config/powerline/segments.py
+++ _config/powerline/segments.py
@@ -7,13 +7,20 @@ def pyenv(*a, **k):
 	try:
 		env = os.popen("pyenv local 2>&1").read().strip()
 		if env.find("no local version configured") >= 0:
-			return None
-		else:
-			if os.popen("pyenv virtualenvs 2>&1").read().find(env) >=0 :
-				segs.insert(0, "venv")
-				env = "(%s)" % env
-			return [{"contents": env, "highlight_group": segs}]
+			env = os.popen("pyenv global 2>&1").read().strip()
+			if env.find("system")>=0:
+				return None
+		virtualenvs = os.popen("pyenv virtualenvs 2>&1 | cut -d ' ' -f 2- | awk '{print $1}'").read().strip().split("\n")
+		if env in virtualenvs:
+			segs.insert(0, "venv")
+		env = "ℙ %s" % env
+		return [{"contents": env, "highlight_group": segs}]
 	except:
 		pass
 
 	return None
+
+
+if __name__ == "__main__":
+	print(pyenv())
+
