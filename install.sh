e8d6699f75dff2610dd3a5bd837d4f39df923e49
Ali-Akber Saifee <ali@indydevs.org>
2017-10-03 18:47:14 +0800

    Update install script

--- install.sh
+++ install.sh
@@ -42,10 +42,5 @@ else
 
 fi
 
-git submodule sync
 git submodule init
-git submodule update
-git submodule foreach git pull origin master
-git submodule foreach git submodule init
-git submodule foreach git submodule update
 pushd .
