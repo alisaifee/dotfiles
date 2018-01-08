56bc5d54c4f5614664a886dc692c1fd2aee02ee8
Ali-Akber Saifee <ali@indydevs.org>
2018-01-08 09:13:16 +0800

    Update tilix theme to use gruvbox dark

--- _shell/path.zsh
+++ _shell/path.zsh
@@ -1,11 +1,9 @@
-export PYTHONPATH=$PYTHONPATH:~/.vim/bundle/powerline:~/.vim/bundle/powerline_mem_segment
 export PYENV_ROOT="$HOME/.pyenv"
 export NODENV_ROOT="$HOME/.nodenv"
 path=($NODENV_ROOT/bin $path)
 path=($PYENV_ROOT/bin $path)
 path=($HOME/.rbenv/bin $path)
 path=($HOME/.cargo/bin $path)
-path=($path ~/.vim/bundle/powerline/scripts)
 if type "go" > /dev/null 2>&1; then
     export GOPATH=~/gohome
     export GOROOT=$(go env GOROOT)
