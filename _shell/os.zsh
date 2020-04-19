8b70b3bce7c2bd4d938582f7137a633f3c34ae45
Ali-Akber Saifee <ali@indydevs.org>
2020-04-19 12:03:00 -0700

    Add aliases for clipboard

--- _shell/os.zsh
+++ _shell/os.zsh
@@ -3,6 +3,8 @@ then
     /usr/bin/keychain -q $HOME/.ssh/id_rsa
     source $HOME/.keychain/`hostname`-sh
     eval `dircolors ~/.dircolors`
+    alias copy='xclip -selection clipboard'
+    alias paste='xclip -o -selection clipboard'
 else
     eval `gdircolors ~/.dircolors`
     alias ctags="`brew --prefix`/bin/ctags"
@@ -11,4 +13,6 @@ else
         alias terminal-notifier='reattach-to-user-namespace terminal-notifier'
         alias ssh='reattach-to-user-namespace ssh'
     fi;
+    alias copy='pbcopy'
+    alias paste='pbpaste'
 fi;
