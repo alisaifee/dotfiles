4316c2f705cacac005185cedcedc3b9c1c42edc2
Ali-Akber Saifee <ali@indydevs.org>
2020-04-19 12:02:27 -0700

    Tweak speedtest script for i3status

--- _i3/speedtest.sh
+++ _i3/speedtest.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-speedtest --simple | grep -E "Download|Upload" | tr '\n' ' ' | sed -e 's/Download://' | sed -e 's/Upload://' | sed -e 's/Mbit\/s//g'
+downicon=
+upicon=祝
+speedtest --simple | grep -E "Download|Upload" | tr '\n' ' ' | sed -e "s/Download\:/$downicon/" | sed -e "s/Upload\:/$upicon/" | sed -e 's/Mbit\/s//g' | sed -e 's/^[[:space:]]*//'
 
