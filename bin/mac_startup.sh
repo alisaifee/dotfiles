559dd38a9259e1db70dcde3ae43010b513d1af43
Ali-Akber Saifee <ali@indydevs.org>
2020-04-29 13:47:29 -0700

    Add mac startup script

---
+++ bin/mac_startup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+/Applications/Firefox.app/Contents/MacOS/firefox --new-window http://mail.indydevs.org &
+/Applications/Kitty.app/Contents/MacOS/kitty glances &
+open -a Whatsapp.app
+open -a Contexts.app
+open -a 'YouTube Music Desktop App.app'
