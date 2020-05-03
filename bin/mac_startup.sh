7ae6fd101d31e570733892cc1933f14b68b84c75
Ali-Akber Saifee <ali@indydevs.org>
2020-05-03 00:01:05 -0700

    Change mac startup script to automator

--- bin/mac_startup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-/Applications/Firefox.app/Contents/MacOS/firefox --new-window http://mail.indydevs.org &
-/Applications/Kitty.app/Contents/MacOS/kitty glances &
-open -a Whatsapp.app
-open -a Contexts.app
-open -a 'YouTube Music Desktop App.app'
-sleep 10
-launchctl kickstart -k "gui/${UID}/homebrew.mxcl.yabai"
