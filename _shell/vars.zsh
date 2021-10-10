69b093d1bfbd27778bef80cbfa460ab5fb2bdce3
Ali-Akber Saifee <ali@indydevs.org>
2021-10-09 17:10:13 -0700

    Remove unused env vars

--- _shell/vars.zsh
+++ _shell/vars.zsh
@@ -1,11 +1,10 @@
 export ZENV=zsh
 export ZENV=prezto
 export ZSH=$HOME/.oh-my-zsh
-export ZSH_THEME=agnoster
 export EDITOR=vim
 export TERM=xterm-256color
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.utf8
 export GPG_TTY=$(tty)
+export AWS_SDK_LOAD_CONFIG=true
 unsetopt nomatch
-[ -f ~/.shell/secrets.zsh ] && source ~/.shell/secrets.zsh
